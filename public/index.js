91516075e74443bdc399c7c7ed2faa11682ae2b0
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-05-31 16:21:45 -0300

    Modificación en archivos

---
+++ public/index.js
@@ -0,0 +1,11 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+// const contenedorVideo = document.getElementById("videoContainer");
+
+// appNode.addEventListener("click",playVideo)
+
+//     window.alert("Hola!")
+// function playVideo()
--- snowpack.config.js
+++ snowpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     src: '/_dist_',
   },
   buildOptions: {
-    baseUrl: '',
+    baseUrl: 'https://Vale-78.github.io/Video-ManipDom',
   },
 }
